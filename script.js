c70c029a873a7f3b6bdc8edee9676df9b4fd9732
Gregory George <gregorycodes210@gmail.com>
2022-09-27 09:52:19 -0500

    JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,80 @@
+// 1.
+console.log(7);
+
+// 2.
+/*
+Number
+String
+Boolean
+Null
+Undefined
+*/
+
+// 3.
+/*
+Addition is +
+Subtract is -
+Multiplication is *
+Division is /
+Modulus is %
+Exponentiation **
+
+Extra
+Increment ++
+Decrement --
+*/
+
+// 4.
+console.log(5 % 2); // 1
+console.log(5 % 4); // 1
+
+// Extra
+console.log(4 % 5); // 4
+
+// 5.
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6.
+console.log("Hawk" + "eye");
+
+// 7.
+console.log("Gregory".length + "George".length); // 13
+
+// 8.
+console.log("Avengers: Endgame"[16]);
+
+// Bonus:
+// 1.
+console.log("\/");
+
+// 2.
+console.log(5 ** 4 - 9); // 616
+
+// 3.
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// 1 - 2 + 4 - 3 = 0
+// 10 * 0 = 0
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5.
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
+// Let Version
+// let m = 25;
+// let c = 4;
+// let u = m + c;
+// console.log(u);
+a = 1
+console.log(a);
